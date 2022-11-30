b440794802c317ed0e6ca50e79fc1c8ab732502d
Tesla Oppong <teslaoppong@gmail.com>
2022-11-30 02:03:13 +0100

    testing

---
+++ test/test_models/test_review.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Unittest for review class: class and methods"""
+
+import unittest
+from models import review
+from models.review import Review
+
+
+class TestDocsReview(unittest.TestCase):
+    """test docstrings for base and test_base files"""
+
+    def test_module(self):
+        """check module docstrings"""
+        self.assertTrue(len(review.__doc__) > 0)
+
+    def test_class(self):
+        """check class docstrings"""
+        self.assertTrue(len(Review.__doc__) > 0)
+
+    def test_method(self):
+        """check method docstrings"""
+        for func in dir(Review):
+            self.assertTrue(len(func.__doc__) > 0)
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ test/test_models/test_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Unittest for state class: class and methods"""
+
+import unittest
+from models import state
+from models.state import State
+
+
+class TestDocsState(unittest.TestCase):
+    """test docstrings for base and test_base files"""
+
+    def test_module(self):
+        """check module docstrings"""
+        self.assertTrue(len(state.__doc__) > 0)
+
+    def test_class(self):
+        """check class docstrings"""
+        self.assertTrue(len(State.__doc__) > 0)
+
+    def test_method(self):
+        """check method docstrings"""
+        for func in dir(State):
+            self.assertTrue(len(func.__doc__) > 0)
+
+if __name__ == "__main__":
+    unittest.main()
